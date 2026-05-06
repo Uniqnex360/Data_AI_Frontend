c6369f210fba9bf0064df91f65d39c495ead6458
Harisankar <harisankar@uniqnex360.com>
2026-05-06 11:15:58 +0530

    fix:Added pixels

--- src/components/AggregationTab.tsx
+++ src/components/AggregationTab.tsx
@@ -1391,7 +1391,7 @@ export default function AggregationTab({
         </div>
         <div
           className="overflow-auto"
-          style={{ height: "calc(100vh - 350px)" }}
+          style={{ height: "calc(100vh - 420px)" }}
         >
           <table className="w-full">
             <thead className="sticky top-0 z-30 bg-slate-50">
